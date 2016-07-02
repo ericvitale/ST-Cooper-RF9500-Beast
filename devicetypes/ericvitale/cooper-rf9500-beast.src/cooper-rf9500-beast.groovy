53198dc9e8b252608ae1d1cc48ea8486559ce9d6
Eric Vitale <ericvitale@gmail.com>
2016-07-01 20:20:42 -0400

    Updated comments at the beginning of the file.

--- devicetypes/ericvitale/cooper-rf9500-beast.src/cooper-rf9500-beast.groovy
+++ devicetypes/ericvitale/cooper-rf9500-beast.src/cooper-rf9500-beast.groovy
@@ -1,3 +1,25 @@
+/**
+ *  Copyright 2015 ericvitale@gmail.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ * 
+ *  This device handler was written specifically for the Cooper RF9500 (RF Battery Operated Switch).
+ *  The version # of the switches I tested with is 3.11 as listed on th back of the switch.
+ *  FCC ID: UH2-RF9500
+ *  IC: 4706C-RF9500
+ *
+ *  You can find this devices handler @ https://github.com/ericvitale/ST-CooperRF9500Beast
+ *  You can find my other device handlers & SmartApps @ https://github.com/ericvitale
+ *
+ */
+
 metadata {
 	definition (name: "Cooper RF9500 Beast", namespace: "ericvitale", author: "ericvitale@gmail.com") {
 		capability "Switch"
